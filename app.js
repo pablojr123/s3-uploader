3817c65d00abebec562e70888083f85a3b09f7f7
pablojr123 <gabijh255@gmail.com>
2025-04-19 06:52:12 -0700

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const upload = multer({ dest: 'uploads/' });
+
+// Set multer untuk memproses file upload
+const storage = multer.memoryStorage(); // Gunakan memory storage supaya file disimpan langsung di memory
+const upload = multer({ storage: storage });
 
 const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
@@ -29,16 +32,15 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
-  const fileContent = fs.readFileSync(req.file.path);
-
   const params = {
     Bucket: process.env.S3_BUCKET_NAME,
-    Key: req.file.originalname,
-    Body: fileContent,
+    Key: req.file.originalname, // Gunakan nama file yang diupload
+    Body: req.file.buffer, // Menggunakan file yang ada di memory
+    ContentType: req.file.mimetype, // Tentukan jenis konten file
+    ACL: 'public-read', // Atur akses sesuai kebutuhan (public-read jika ingin file bisa diakses publik)
   };
 
   s3.upload(params, (err, data) => {
-    fs.unlinkSync(req.file.path); // hapus file lokal setelah upload
     if (err) {
       return res.send("Upload gagal: " + err);
     }
